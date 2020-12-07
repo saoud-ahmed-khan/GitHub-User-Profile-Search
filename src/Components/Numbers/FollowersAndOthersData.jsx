e2f30327391e767a0327f5c736919871a25503b0
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-07 17:08:36 +0500

    updation

--- src/Components/Numbers/FollowersAndOthersData.jsx
+++ src/Components/Numbers/FollowersAndOthersData.jsx
@@ -9,7 +9,7 @@ export function Followersandothersdata() {
     return (
         <div className="follower-main">
             <div className="followers-logo">
-                <BsPeopleFill color="#364f6b" size="4vw"/>
+                <BsPeopleFill color="#364f6b" size="3.3vw"/>
             </div>
             <div>
                 <div><CountUp className="follower-num" start={0} end={20} duration={4} /></div>
