cce5d0362aa146bc687a6c513e732baf14e94954
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-07 16:10:16 +0500

    adding divs

--- src/Components/Numbers/FollowersAndOthersData.jsx
+++ src/Components/Numbers/FollowersAndOthersData.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import CountUp from 'react-countup';
+import "./Followers.css"
 
 import { BsPeopleFill } from "react-icons/bs";
 export function Followersandothersdata() {
 
 
     return (
-        <div>
-            <div>
-                <BsPeopleFill color="#364f6b" size="3vw"/>
+        <div className="follower-main">
+            <div className="followers-logo">
+                <BsPeopleFill color="#364f6b" size="3.5vw"/>
             </div>
             <div>
-
+                <div><CountUp className="follower-num" start={0} end={20} duration={4} /></div>
+                <div>Followers</div>
+                
             </div>
         </div>
     )
--- src/Components/Numbers/RenderingNumbers.jsx
+++ src/Components/Numbers/RenderingNumbers.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Followersandothersdata } from "./FollowersAndOthersData";
-
 export function Renderingnumbers(props) {
     
 
     return (
-        <>
+        <div className="rendering-main">
+          <Followersandothersdata/>
+          <Followersandothersdata/>
+          <Followersandothersdata/>
           <Followersandothersdata/>  
-        </>
+  
+  
+  
+        </div>
     )
 }
