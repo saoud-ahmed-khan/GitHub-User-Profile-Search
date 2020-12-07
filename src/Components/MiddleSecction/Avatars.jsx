e50e07eddf157b9f591dff0e838faa3a8be0715b
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-08 00:39:24 +0500

    round shape pic

--- src/Components/MiddleSecction/Avatars.jsx
+++ src/Components/MiddleSecction/Avatars.jsx
@@ -7,7 +7,7 @@ export function Avatars() {
 
     return (
         <div className="main-Avatars">
-            <div><img src="https://avatars3.githubusercontent.com/u/60979682?v=4" width="100%" alt="profile dp"/></div>
+            <div ><img  className="Avatars-pic" src="https://avatars3.githubusercontent.com/u/60979682?v=4" width="100%" alt="profile dp"/></div>
             <div><Namedata Icon={BsPersonSquare} Name="Here the name comes" /></div>
             <div><Namedata Icon={BsPersonSquare} Name="Here the name comes" /></div>
             <div><Namedata Icon={BsPersonSquare} Name="Here the name comes" /></div>
