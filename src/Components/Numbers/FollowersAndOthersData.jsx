e8e2366679121bf0d4499a379f78844098e0f06b
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-07 16:17:59 +0500

    adding styling

--- src/Components/Numbers/FollowersAndOthersData.jsx
+++ src/Components/Numbers/FollowersAndOthersData.jsx
@@ -9,7 +9,7 @@ export function Followersandothersdata() {
     return (
         <div className="follower-main">
             <div className="followers-logo">
-                <BsPeopleFill color="#364f6b" size="3.5vw"/>
+                <BsPeopleFill color="#364f6b" size="3vw"/>
             </div>
             <div>
                 <div><CountUp className="follower-num" start={0} end={20} duration={4} /></div>
