b76d9a681998e0f94cd93e183963a1f6a7d49153
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-08 23:18:05 +0500

    adding content

--- src/Components/MiddleSecction/TextData.jsx
+++ src/Components/MiddleSecction/TextData.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Avatars.css"
 export function Textdata(props) {
-    
+
 
     return (
         <div className="textdata-main">
             
+                <div className="textData-text">User Name: Saoud</div>
+                <div>Bio: cloud native app</div>
+                <div>Location: KArachi</div>
+                <div>Email: saoudkhan987@gmail.com</div>
+                <div>Company: PanaCloud</div>
+                <div>Blog: ajsfajsfashahgf</div>
+                <div>Twitter User Name: asdfasdfasdfaf</div>
+                
+           
         </div>
     )
 }
