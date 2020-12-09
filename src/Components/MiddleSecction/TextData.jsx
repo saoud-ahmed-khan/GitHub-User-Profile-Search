bd4c9bf0209271490547a708bb08185a966d88f2
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-09 20:50:30 +0500

    adding responsiveness

--- src/Components/MiddleSecction/TextData.jsx
+++ src/Components/MiddleSecction/TextData.jsx
@@ -6,13 +6,13 @@ export function Textdata(props) {
     return (
         <div className="textdata-main">
             
-                <div className="textData-text">User Name: Saoud</div>
-                <div>Bio: cloud native app</div>
-                <div>Location: KArachi</div>
-                <div>Email: saoudkhan987@gmail.com</div>
-                <div>Company: PanaCloud</div>
-                <div>Blog: ajsfajsfashahgf</div>
-                <div>Twitter User Name: asdfasdfasdfaf</div>
+                <div><span>user_name:</span> Saoud</div>
+                <div><span>Bio: </span>cloud native app</div>
+                <div><span>Location: </span>KArachi</div>
+                <div><span>Email: </span>saoudkhan987@gmail.com</div>
+                <div><span>Company: </span>PanaCloud</div>
+                <div><span>Blog: </span>ajsfajsfashahgf</div>
+                <div><span>Twitter User Name: </span>asdfasdfasdfaf</div>
                 
            
         </div>
